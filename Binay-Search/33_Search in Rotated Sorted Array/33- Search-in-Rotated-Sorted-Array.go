2c8cd4ae6e0b7123550385fda92628f7b72e1a69
shaohguo <shaohe.guo@oracle.com>
2020-11-03 13:51:21 +0800

    Update 33- Search-in-Rotated-Sorted-Array.go

--- Binay-Search/33_Search in Rotated Sorted Array/33- Search-in-Rotated-Sorted-Array.go
+++ Binay-Search/33_Search in Rotated Sorted Array/33- Search-in-Rotated-Sorted-Array.go
@@ -2,25 +2,25 @@ package BinarySearch
 
 func Search(nums []int, target int) int {
 
-	binarySearchFunc := func(l, r int) int {
-		var (
-			tmpLeft int = l
-			tmpRight int = r
-		)
-
-		for tmpLeft <= tmpRight {
-			tmpMid := tmpLeft + (tmpRight - tmpLeft) / 2
-
-			if target == nums[tmpMid] {
-				return tmpMid
-			} else if target < nums[tmpMid] {
-				tmpRight = tmpMid - 1
-			} else if target > nums[tmpMid] {
-				tmpLeft = tmpMid + 1
-			}
- 		}
- 		return -1
-	}
+	//binarySearchFunc := func(l, r int) int {
+	//	var (
+	//		tmpLeft int = l
+	//		tmpRight int = r
+	//	)
+	//
+	//	for tmpLeft <= tmpRight {
+	//		tmpMid := tmpLeft + (tmpRight - tmpLeft) / 2
+	//
+	//		if target == nums[tmpMid] {
+	//			return tmpMid
+	//		} else if target < nums[tmpMid] {
+	//			tmpRight = tmpMid - 1
+	//		} else if target > nums[tmpMid] {
+	//			tmpLeft = tmpMid + 1
+	//		}
+ 	//	}
+ 	//	return -1
+	//}
 
 	var binaryDivideFunc func(int, int) int
 	binaryDivideFunc = func(L, R int) int {
@@ -40,13 +40,13 @@ func Search(nums []int, target int) int {
 
 		if nums[left] <= nums[mid] {
 			if target >= nums[left] && target < nums[mid] {
-				return binarySearchFunc(left, mid - 1)
+				return binaryDivideFunc(left, mid - 1)
 			} else {
 				return binaryDivideFunc(mid + 1, right)
 			}
 		} else if nums[right] >= nums[mid] {
 			if target > nums[mid] && target <= nums[right] {
-				return binarySearchFunc(mid + 1, right)
+				return binaryDivideFunc(mid + 1, right)
 			} else {
 				return binaryDivideFunc(left, mid - 1)
 			}
