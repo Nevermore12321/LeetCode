c68015410d8d2885d72be8f1e1a3a961c270a887
Shaohe Guo <842125706@qq.com>
2022-07-23 17:05:22 +0800

    add 88 283

---
+++ Array/88_Merge_Sorted_Array/88_Merge_Sorted_Array.go
@@ -0,0 +1,15 @@
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	var (
+		i int = m - 1
+		j int = n - 1
+	)
+	for k := m + n - 1; k >= 0; k-- {
+		if i < 0 || (j >= 0 && nums1[i] <= nums2[j]) {
+			nums1[k] = nums2[j]
+			j--
+		} else if j < 0 || (i >= 0 && nums1[i] > nums2[j]) {
+			nums1[k] = nums1[i]
+			i--
+		}
+	}
+}
